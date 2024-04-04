51ae52c1795ba02f9d51422ac94919fe372618b8
karki <karkisayal7@gmail.com>
2024-04-04 07:41:53 +0545

    Initial COmmit by Sayal

--- lib/View/Login.dart
+++ lib/View/Login.dart
@@ -1,210 +1,102 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
+import 'package:flutter/material.dart';
 class Login extends StatefulWidget {
   @override
-  State<Login> createState() => _LoginState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginState extends State<Login> {
-  final _formKey = GlobalKey<FormState>();
+class _LoginPageState extends State<Login> {
+  bool _isPasswordVisible = false;
 
-  bool? isChecked=false;
+  void _togglePasswordVisibility() {
+    setState(() {
+      _isPasswordVisible = !_isPasswordVisible;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.withOpacity(0.3),
-      body: Center(
-        child: Container(
-          width:kIsWeb? MediaQuery.of(context).size.width / 1.3:MediaQuery.of(context).size.width ,
-          height:kIsWeb? MediaQuery.of(context).size.height / 1.3:MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              return Wrap(
-                children: [
-                  FractionallySizedBox(
-                    widthFactor: kIsWeb ? 0.5:1,
-                    child: Container(
-                      height: constraints.maxHeight,
-                      width: constraints.maxWidth,
-                      decoration: BoxDecoration(
-                        color: Colors.blueAccent.withOpacity(0.2),
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(15),
-                          bottomLeft: Radius.circular(15),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 20, left: 10),
-                        child: Form(
-                          key: _formKey,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              kIsWeb
-                                  ? SizedBox.shrink()
-                                  : SizedBox(
-                                height: 30,
-                              ),
-                              Text(
-                                'Login',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w800,
-                                    fontSize: 15,
-                                    color: Colors.blueGrey),
-                              ),
-                              const SizedBox(
-                                height: 7,
-                              ),
-                              RichText(
-                                text: TextSpan(
-                                  text: 'Doesn\'t have an account yet? ',
-                                  style: TextStyle(
-                                    fontSize: 8,
-                                    color: Colors.grey,
-                                  ),
-                                  children: <TextSpan>[
-                                    TextSpan(
-                                      text: 'SIGN UP',
-                                      style: TextStyle(
-                                        color: Colors.pink.withOpacity(0.5),
-                                        decoration: TextDecoration.underline,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              SizedBox(
-                                height: 25,
-                              ),
-                              Text(
-                                'Email Address',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              Padding(
-                                padding:
-                                const EdgeInsets.only(right: 40, top: 5),
-                                child: TextFormField(
-                                  keyboardType: TextInputType.emailAddress,
-                                  decoration: InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    hintText: 'you@example.com',
-                                    hintStyle: TextStyle(fontSize: 15),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 25,
-                              ),
-                              Wrap(
-                                children: [
-                                  FractionallySizedBox(
-                                    widthFactor: 0.3,
-                                    child: Text(
-                                      'Password',
-                                      style: TextStyle(
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    ),
-                                  ),
-                                  FractionallySizedBox(
-                                    widthFactor: 0.4,
-                                  ),
-                                  FractionallySizedBox(
-                                    widthFactor: 0.3,
-                                    child: Text(
-                                      'Forget Password?',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.w800,
-                                        fontSize: 12,
-                                        decoration: TextDecoration.underline,
-                                        color: Colors.pink.withOpacity(0.8),
-                                      ),
-                                    ),
-                                  ),
-
-                                ],
-                              ),
-                              Padding(
-                                padding:
-                                const EdgeInsets.only(right: 40, top: 5),
-                                child: TextFormField(
-                                  keyboardType: TextInputType.visiblePassword,
-                                  obscureText: true,
-                                  obscuringCharacter: '*',
-                                  decoration: InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    hintText: 'password',
-                                    hintStyle:TextStyle(fontSize: 15),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Checkbox(
-                                value: isChecked,
-                                activeColor: Colors.blueAccent,
-                                tristate: true,
-                                onChanged: (newBool){
-                                  setState((){
-                                    isChecked=newBool;
-                                  });
-                                },
-                              ),
-                              SizedBox(
-                                width: constraints.maxWidth,
-                                child: Padding(
-                                  padding: const EdgeInsets.only(right: 20),
-                                  child: ElevatedButton(
-                                    style:ElevatedButton.styleFrom(elevation: 6,backgroundColor: Colors.green),
-                                    onPressed: ()=>{
-                                      //Navigator.pushNamed(context, RouteConstants.dashRoute),
-                                    },
-                                    child: Text('Login'),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
+      body: Container(
+        color: Theme.of(context).primaryColor,
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Login',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 10.0),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Username',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
                       ),
                     ),
                   ),
-                  kIsWeb ?
-                  FractionallySizedBox(
-                    widthFactor: 0.5,
-                    child: Container(
-                      padding: EdgeInsets.all(30),
-                      decoration: BoxDecoration(
-                        color: Colors.blueAccent.withOpacity(0.4),
-                        borderRadius: const BorderRadius.only(
-                          bottomRight: Radius.circular(15),
-                          topRight: Radius.circular(15),
+                ],
+              ),
+              SizedBox(height: 10.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.lock,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 10.0),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Password',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                            color: Colors.black,
+                          ),
+                          onPressed: _togglePasswordVisibility,
                         ),
                       ),
-                      height: constraints.maxHeight,
-                      width: constraints.maxWidth,
-                      child: Center(
-                        child: SvgPicture.network('assets/images/Leftside.svg'),
-                      ),
+                      obscureText: !_isPasswordVisible,
                     ),
-                  )
-                      :SizedBox.shrink(),
+                  ),
                 ],
-              );
-            },
+              ),
+              SizedBox(height: 20.0),
+              SizedBox(
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).hintColor,
+                  ),
+                  child: Text('Login'),
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'View/Login.dart';
 import 'View/route_constants.dart';
-
+import 'firebase_options.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  initFirebase();
+  runApp(MaterialApp(
+    title: 'Flutter App!!',
+    theme: ThemeData(
+        primaryColor: Color(0xFF6D4C41), // Use the color of your furniture items
+        hintColor: Color(0xFF8D6E63), // Use a complementary color
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+
+    ),
+
+    debugShowCheckedModeBanner: false,
+    initialRoute: RouteConstants.loginRoute,
+    routes: {
+      RouteConstants.loginRoute: (context) => Login(),
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter App!!',
-      theme: ThemeData(
-        colorSchemeSeed: Colors.indigo,
-        useMaterial3: false,
-        brightness: Brightness.light,
-      ),
-      darkTheme: ThemeData(
-        colorSchemeSeed: Colors.blue,
-        useMaterial3: false,
-        brightness: Brightness.dark,
-      ),
-      debugShowCheckedModeBanner: false,
-      initialRoute: RouteConstants.loginRoute,
-      routes: {
-        RouteConstants.loginRoute:(context)=>Login(),
-      },
-    );
-  }
+initFirebase()async{
+  await Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform);
 }
